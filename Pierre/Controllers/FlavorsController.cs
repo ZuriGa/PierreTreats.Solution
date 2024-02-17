d070f5355afc06dc1b74169dadcf9fc52a5d4328
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-17 09:47:31 -0800

    add delete action + view file

--- Pierre/Controllers/FlavorsController.cs
+++ Pierre/Controllers/FlavorsController.cs
@@ -56,6 +56,21 @@ namespace Pierre.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Delete(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
 
   }
 }
