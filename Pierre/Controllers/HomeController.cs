4a4dc32df1bf694d3518692c127c53cde3f12b61
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-17 22:03:23 -0800

    add updated home view + home controller

--- Pierre/Controllers/HomeController.cs
+++ Pierre/Controllers/HomeController.cs
@@ -1,13 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Pierre.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Pierre.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierreContext _db;
+
+    public HomeController(PierreContext db)
+    {
+      _db = db;
+    }
+
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Flavor[] flavors = _db.Flavors.ToArray();
+      Treat[] treats = _db.Treats.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("flavors", flavors);
+      model.Add("treats", treats);
+      return View(model);
     }
   }
 }
