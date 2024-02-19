625cb48837252ba45028f61a4718fcd2ab002243
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-18 21:44:10 -0800

    add styling to flavors view

--- Pierre/Models/Flavor.cs
+++ Pierre/Models/Flavor.cs
@@ -8,7 +8,7 @@ namespace Pierre.Models
   public class Flavor
   {
     public int FlavorId { get; set; }
-    [Display(Name = "Flavor Name")]
+    [Display(Name = "Flavor Name ")]
     [Required(ErrorMessage = "Flavor name is required")]
     public string FlavorName { get; set; }
     public List<FlavorTreat> JoinEntities { get; set; }
